b1ef89c841096328cdf254cf0ed7a6be4d72b8b5
Kang R <113736304+kangrdev@users.noreply.github.com>
2023-11-30 18:31:44 +0800

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -12,7 +12,7 @@
         <q-input v-model="text" type="text" label="Username" />
         <q-input v-model="pw" type="password" label="Password" />
         <q-btn color="red" label="Masuk" @click="onClick" style="margin-top: 20px; width: 100%;" to="/home" />
-        <h6 class="text-center">Power By SMK Telkom</h6>
+        <h6 class="text-center">Power By Kang R</h6>
       </q-card-section>
     </q-card>
     </div>
